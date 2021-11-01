024411bb745a6e6b66c2bbf5e8c59a37307db6f6
Arthur <arthur.vallee@gmail.com>
2021-11-01 09:45:49 +0100

    Creation  des classes

--- ProjetPAA-S5-Pirates/src/Pirate.java
+++ ProjetPAA-S5-Pirates/src/Pirate.java
@@ -1,4 +1,9 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 
 public class Pirate {
-
+	private String nom;
+	private ArrayList<Objet> prefObj;
+	private HashMap<Pirate, Integer> relations;
 }
