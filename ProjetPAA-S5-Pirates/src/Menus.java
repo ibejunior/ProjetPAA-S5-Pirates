54cae511d3e75b39491d5a1da1253c96d8701793
Arthur <arthur.vallee@gmail.com>
2021-11-01 14:03:42 +0100

    Menus et Distribution

--- ProjetPAA-S5-Pirates/src/Menus.java
+++ ProjetPAA-S5-Pirates/src/Menus.java
@@ -1,30 +1,55 @@
 import java.util.Scanner;
 
 public class Menus {
-	private Scanner sc;
-	private int n;
-	
-	public Menus() {
-		this.n = 0;
-		sc = new Scanner(System.in);
-	}
-	
-	public void initialisations() {
-		System.out.println("wsh bien ou quoi ?");
-		System.out.println("Combien de pirates constituent l'équipage ?");
-		n = sc.nextInt();
-		while(n < 2 || n > 26) {
-			System.out.println("Vous avez donné un nombre de pirates inférieur à 2 ou supérieur à 26. Veuillez donner un "
-					+ "nombre de pirates adéquat");
-			n = sc.nextInt();
-		}
-		System.out.println("L'équipage est composé de " + n + " pirates et ils ont trouvé " + n + " objets.");
-		Distribution distrib = new Distribution(n);
-		distrib.initPirates(n);
-	}
-	
-	public void gestionPirate() {
-		System.out.println("Veuillez choisir une option :");
-		System.out.println("\t\t1) ajouter une relation");
-	}
+    private Scanner sc;
+    private int n;
+    private Distribution distrib;
+
+    public Menus() {
+        this.distrib = null;
+        this.n = 0;
+        sc = new Scanner(System.in);
+    }
+
+    public void initialisations() {
+        System.out.println("wsh bien ou quoi ?");
+        System.out.println("Combien de pirates constituent l'équipage ?");
+        n = sc.nextInt();
+        while(n < 2 || n > 26) {
+            System.out.println("Vous avez donné un nombre de pirates inférieur à 2 ou supérieur à 26. Veuillez donner un "
+                    + "nombre de pirates adéquat");
+            n = sc.nextInt();
+        }
+        System.out.println("L'équipage est composé de " + n + " pirates et ils ont trouvé " + n + " objets.");
+        distrib = new Distribution(n);
+        distrib.initPirates();
+    }
+
+    public void gestionPirate() {
+        System.out.println("Veuillez choisir une option :");
+        String s1,s2;
+        Pirate p1,p2;
+        distrib.initRelations();
+        System.out.println("\t\t1) ajouter une relation");
+        n = sc.nextInt();
+        switch(n) {
+        case 1 : 
+            System.out.println("Donner deux pirates qui s'aiment pas");
+            s1 = sc.next();
+            s2 = sc.next();
+            p1 = distrib.cherchePirate(s1);
+            if (p1 == null) {
+                System.out.println("Le pirate "+s1+" n'existe pas");
+            }
+            p2 = distrib.cherchePirate(s2);
+            if (p2 == null) {
+                System.out.println("Le pirate "+s2+" n'existe pas");
+            }
+            distrib.gestionRelations(p1, p2);
+
+
+
+        }
+    }
+
 }
