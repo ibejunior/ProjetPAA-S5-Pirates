e1e4ee0cdca51e69508561bda99a683b80dfabe3
Arthur <arthur.vallee@gmail.com>
2021-11-01 10:04:03 +0100

    Suite de la création des classes

--- ProjetPAA-S5-Pirates/src/Objet.java
+++ ProjetPAA-S5-Pirates/src/Objet.java
@@ -1,4 +1,16 @@
 
 public class Objet {
 	private String nom;
+	
+	public Objet(String nom) {
+		this.nom = nom;
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+	
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
 }
